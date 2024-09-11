e5a2b3fa600a306962d84339b2488bbefee5bc62
HonoratoOmar67 <omarhonorato03@gmail.com>
2024-09-11 13:25:06 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#you got this Omar, don't overthink too much.
+
+
+ # additional charge for use per hour after 1000
+firstHours = 1000
+
+if 1500 >= firstHours:
+    print("Amount owed is $122.625")
+if 764 >= firstHours:
+    print("Amount owed an extra amount")
+else:
+    print("Amounted owed is $58.31612")
+if 1215 >= firstHours:
+    print("Amounted owed is $96.23685")
+if 812 >= firstHours:
+    print("Owed an extra amount")
+else:
+    print("Amounted owed is $61.97996")
+
+
+
+
+
+
+
+
+
+
+
